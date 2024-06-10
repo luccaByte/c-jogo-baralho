d6acd760a7d9cd6dc399f1191d03c692f39ddee6
lucas <lucaston2323@gmail.com>
2024-06-10 19:45:37 -0300

    fix: em funções / feat: função para ter mais detalhes visual

--- C-JogoBaralho/App.cpp
+++ C-JogoBaralho/App.cpp
@@ -1,16 +1,20 @@
 #include <stdio.h>
-#include <conio.h>
-#include <stdlib.h> //malloc, free
-#include <time.h> // gerar numeros aleatorios
+#include <stdlib.h> // malloc, free
+#include <time.h>   // gerar números aleatórios
 #include <string.h>
+#include <locale.h>
+
+#define TAMANHO_MAX 52
+#define _CRT_SECURE_NO_WARNINGS
+
 
 typedef struct 
 {
-    int num; 
+    int num;
     char naipe[10];
 } Carta;
 
-typedef struct
+typedef struct 
 {
     Carta* cartas;
     int tamanho;
@@ -24,48 +28,37 @@ void push(Pilha* p, Carta c);
 Carta pop(Pilha* p);
 void gerarBaralho(Pilha* monte);
 void embaralhar(Pilha* monte);
-void simularJogo(Pilha* monte, Pilha* morto, Pilha* jogador1, Pilha* jogador2, int* numerosJ1, int* numerosJ2);
+void jogar(Pilha* monte, Pilha* jogador1, Pilha* jogador2);
 
-
-Pilha* monte;
-Pilha* morto;
-Pilha* jogador1;
-Pilha* jogador2;
-
-
-int main()
+int main() 
 {
-    srand(time(NULL));
+    setlocale(LC_ALL, "Portuguese");
 
-    monte = criar_pilha(52);
-    morto = criar_pilha(52);
-    jogador1 = criar_pilha(52);
-    jogador2 = criar_pilha(52);
+    Pilha* monte = criar_pilha(TAMANHO_MAX);
+    Pilha* jogador1 = criar_pilha(TAMANHO_MAX);
+    Pilha* jogador2 = criar_pilha(TAMANHO_MAX);
 
     gerarBaralho(monte);
+    srand((unsigned int)time(NULL));
+
     embaralhar(monte);
-
-    int numerosJ1[5] = { 1, 2, 3, 4, 5 }; 
-    int numerosJ2[5] = { 6, 7, 8, 9, 10 };
-
-    simularJogo(monte, morto, jogador1, jogador2, numerosJ1, numerosJ2);
+    jogar(monte, jogador1, jogador2);
 
     clean(monte);
-    clean(morto);
     clean(jogador1);
     clean(jogador2);
+
+    return 0;
 }
 
-
-Pilha* criar_pilha(int capacidade)
+Pilha* criar_pilha(int capacidade) 
 {
     Pilha* p = (Pilha*)malloc(sizeof(Pilha));
-    p->cartas = (Carta*)malloc(capacidade* sizeof(Carta));
+    p->cartas = (Carta*)malloc(capacidade * sizeof(Carta));
     p->tamanho = 0;
     p->capacidade = capacidade;
     return p;
 }
-
 
 void clean(Pilha* p) 
 {
@@ -73,19 +66,19 @@ void clean(Pilha* p)
     free(p);
 }
 
-int empty(Pilha* p)
+int empty(Pilha* p) 
 {
     return p->tamanho == 0;
 }
 
-void push(Pilha* p, Carta c)
+void push(Pilha* p, Carta c) 
 {
-    if (p->tamanho < p->capacidade)
+    if (p->tamanho < p->capacidade) 
     {
         p->cartas[p->tamanho] = c;
         p->tamanho++;
     }
-    else
+    else 
     {
         printf("Erro: a pilha está cheia!\n");
     }
@@ -101,80 +94,96 @@ Carta pop(Pilha* p)
     else 
     {
         printf("Erro: A pilha está vazia!\n");
-        Carta cartaVazia = { 0, "" };
-        return cartaVazia;
+        exit(1);
     }
 }
 
-void gerarBaralho(Pilha* monte) {
-    const char* naipes[] = { "Copas", "Espadas", "Ouros", "Paus" };
-    for (int i = 0; i < 4; i++) 
+void gerarBaralho(Pilha* monte) 
+{
+    int valores[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+    char* naipes[] = { "Copas", "Espadas", "Ouros", "Paus" };
+    for (int i = 0; i < 13; i++) 
     {
-        for (int j = 1; j <= 13; j++) 
+        for (int j = 0; j < 4; j++) 
         {
-            Carta carta;
-            carta.num = j;
-            strcpy_s(carta.naipe, sizeof(carta.naipe), naipes[i]);
-            push(monte, carta);
+            Carta c;
+            c.num = valores[i];
+            strcpy(c.naipe, naipes[j]);
+            push(monte, c);
         }
     }
 }
 
-
 void embaralhar(Pilha* monte) 
 {
-    for (int i = 0; i < monte->tamanho; i++) 
+    int i, j;
+    Carta temp;
+    for (i = 0; i < monte->tamanho; i++) 
     {
-        int j = rand() % monte->tamanho;
-        Carta temp = monte->cartas[i];
+        j = rand() % monte->tamanho;
+        temp = monte->cartas[i];
         monte->cartas[i] = monte->cartas[j];
         monte->cartas[j] = temp;
     }
 }
 
-void simularJogo(Pilha* monte, Pilha* morto, Pilha* jogador1, Pilha* jogador2, int* numerosJ1, int* numerosJ2) 
+void jogar(Pilha* monte, Pilha* jogador1, Pilha* jogador2) 
 {
-    int pontosJ1 = 0, pontosJ2 = 0;
+    int escolhas1[5], escolhas2[5];
+    printf("  ========== JOGO DE CARTAS ==========  \n");
+    printf("Jogador 1, escolha 5 cartas (1 a 13):\n");
+    for (int i = 0; i < 5; i++) 
+    {
+        scanf_s("%d", &escolhas1[i]);
+    }
+    printf("Jogador 2, escolha 5 cartas (1 a 13):\n");
+    for (int i = 0; i < 5; i++) 
+    {
+        scanf_s("%d", &escolhas2[i]);
+    }
+
+    int pontos1 = 0, pontos2 = 0;
+
     while (!empty(monte)) 
     {
-        Carta carta = pop(monte);
-        int achou = 0;
+        Carta c = pop(monte);
+        printf("  ========== Carta retirada: %d de %s ==========  \n", c.num, c.naipe);
 
-        for (int i = 0; i < 5; i++) {
-            if (carta.num == numerosJ1[i]) 
-            {
-                push(jogador1, carta);
-                pontosJ1++;
-                achou = 1;
-                break;
-            }
-            else if (carta.num == numerosJ2[i]) 
-            {
-                push(jogador2, carta);
-                pontosJ2++;
-                achou = 1;
-                break;
-            }
-        }
-
-        if (!achou) 
+        for (int i = 0; i < 5; i++) 
         {
-            push(morto, carta);
+            if (c.num == escolhas1[i]) 
+            {
+                push(jogador1, c);
+                pontos1++;
+                printf("  ========== Jogador 1 ganhou a carta! ==========  \n");
+                break;
+            }
+            else if (c.num == escolhas2[i]) 
+            {
+                push(jogador2, c);
+                pontos2++;
+                printf("  ========== Jogador 2 ganhou a carta! ==========  \n");
+                break;
+            }
         }
+        printf("* Pontos atuais:  \n");
+        printf("- Jogador 1: %d \n", pontos1);
+        printf("- Jogador 2: %d \n", pontos2);
     }
-
-    printf("Pontos Jogador 1: %d\n", pontosJ1);
-    printf("Pontos Jogador 2: %d\n", pontosJ2);
-
-    if (pontosJ1 > pontosJ2) 
+    printf("Fim do jogo!\n");
+    printf("* Pontos finais:\n");
+    printf("- Jogador 1: %d   \n", pontos1);
+    printf("- Jogador 2: %d   \n", pontos2);
+    if (pontos1 > pontos2) 
     {
-        printf("O jogador 1 venceu!\n");
+        printf("  =========== Jogador 1 venceu! ===========  \n");
     }
-    else if (pontosJ2 > pontosJ1) 
+    else if (pontos2 > pontos1) 
     {
-        printf("O jogador 2 venceu!\n");
+        printf("  =========== Jogador 2 venceu! ===========  \n");
     }
-    else {
-        printf("Empate!\n");
+    else 
+    {
+        printf("  ============= EMPATE =============  \n");
     }
 }
